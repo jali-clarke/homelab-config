6c1bc324345a8ba57a3559ece85c82df3d8dc7a9
jali-clarke <GRX01@hotmail.com>
2021-03-13 12:17:14 -0500

    add system configs to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,34 @@
 {
   description = "env for managing bare metal infra";
 
-  outputs = { self, nixpkgs }: {
+  outputs = {self, nixpkgs}: {
+    nixosConfigurations =
+      let
+        nixosSystemFromDir =
+          {system, subdirName}:
+          nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = [./configurations/${subdirName}/configuration.nix];
+          };
+      in
+      {
+        atlas = nixosSystemFromDir {system = "x86_64-linux"; subdirName = "atlas"};
+        bootstrap-bill = nixosSystemFromDir {system = "x86_64-linux"; subdirName = "bootstrap-bill"};
+        nixos-oblivion = nixosSystemFromDir {system = "x86_64-linux"; subdirName = "nixos-oblivion"};
+        speet = nixosSystemFromDir {system = "aarch64-linux"; subdirName = "speet"};
+        weedle = nixosSystemFromDir {system = "x86_64-linux"; subdirName = "weedle"};
+      };
+
     devShell.x86_64-linux =
       let
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
       in pkgs.mkShell {
         name = "bare-metal-shell";
         buildInputs = [
-          pkgs.nixos-generators
+          pkgs.git
           pkgs.kubectl
+          pkgs.nixos-generators
+          pkgs.vim
         ];
       };
   };
