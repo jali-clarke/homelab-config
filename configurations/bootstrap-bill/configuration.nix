e94b4e912be09fba59867126f67e909914a5fa9f
jali-clarke <GRX01@hotmail.com>
2021-03-13 14:37:00 -0500

    seemingly finish up bootstrap-bill

--- configurations/bootstrap-bill/configuration.nix
+++ configurations/bootstrap-bill/configuration.nix
@@ -4,8 +4,8 @@
 
 {pkgs, ...}: {
   imports = [
+    ../../common/common-config.nix
     ../../common/users
-    ../../common/system-packages.nix
     ../../common/zfs-support.nix
   ];
 
@@ -16,32 +16,10 @@
   boot.binfmt.emulatedSystems = ["aarch64-linux"];
 
   networking.hostName = "bootstrap-bill"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  services.openssh.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/Toronto";
-
-  networking.useDHCP = true;
 
   environment.systemPackages = [
     pkgs.nfs-utils
   ];
 
   homelab-config.zfs-support.doAutoScrub = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
-
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
 }
