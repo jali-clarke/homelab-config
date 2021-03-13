0a82e8dcd725b959595a8924539426f6b7c8bb19
jali-clarke <GRX01@hotmail.com>
2021-03-13 14:37:28 -0500

    finish atlas configuration for now

--- configurations/atlas/configuration.nix
+++ configurations/atlas/configuration.nix
@@ -1,3 +1,13 @@
 {config, pkgs, ...}: {
-  imports = 
+  imports = [
+    ./hardware-configuration.nix
+
+    ../../common/common-config.nix
+    ../../common/users
+    ../../common/zfs-support.nix
+  ];
+
+  homelab-config.zfs-support.zfsARCSizeMaxGB = 8;
+
+  networking.hostName = "atlas"; # Define your hostname.
 }
