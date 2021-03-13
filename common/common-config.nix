0b1ea02e3b0fb678126691d3c8998ca691867b3b
jali-clarke <GRX01@hotmail.com>
2021-03-13 14:35:38 -0500

    create common-config.nix

---
+++ common/common-config.nix
@@ -0,0 +1,42 @@
+{...}:
+{
+  networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
+  networking.firewall.enable = false;
+
+  services.openssh.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Toronto";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.eth0.useDHCP = true;
+
+  environment.systemPackages = [
+    pkgs.git
+    pkgs.vim
+  ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+    };
+
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+}
+
