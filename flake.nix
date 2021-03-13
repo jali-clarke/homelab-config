01fcf76de147b03fc7bcb6c5c7b53e30925c9a10
jali-clarke <GRX01@hotmail.com>
2021-03-13 15:14:45 -0500

    add updated nixos-generators

--- flake.nix
+++ flake.nix
@@ -1,37 +1,47 @@
 {
   description = "env for managing bare metal infra";
 
-  outputs = {self, nixpkgs}: {
-    nixosConfigurations =
-      let
-        nixosSystemFromDir =
-          {system, subdirName}:
-          nixpkgs.lib.nixosSystem {
-            inherit system;
-            modules = [
-              (./configurations + "/${subdirName}/configuration.nix")
-            ];
-          };
-      in
-      {
-        atlas = nixosSystemFromDir {system = "x86_64-linux"; subdirName = "atlas";};
-        bootstrap-bill = nixosSystemFromDir {system = "x86_64-linux"; subdirName = "bootstrap-bill";};
-        nixos-oblivion = nixosSystemFromDir {system = "x86_64-linux"; subdirName = "nixos-oblivion";};
-        # speet = nixosSystemFromDir {system = "aarch64-linux"; subdirName = "speet";};
-        # weedle = nixosSystemFromDir {system = "x86_64-linux"; subdirName = "weedle";};
-      };
+  input.nixos-generators.url = "github:nix-community/nixos-generators";
 
-    devShell.x86_64-linux =
-      let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in pkgs.mkShell {
-        name = "bare-metal-shell";
-        buildInputs = [
-          pkgs.git
-          pkgs.kubectl
-          pkgs.nixos-generators
-          pkgs.vim
-        ];
-      };
-  };
+  outputs = {self, nixpkgs, nixos-generators}:
+    let
+      overlay = system:
+        final: prev: {
+          nixos-generators = nixos-generators.defaultPackage.${system};
+        };
+    in
+    {
+      nixosConfigurations =
+        let
+          nixosSystemFromDir =
+            {system, subdirName}:
+            nixpkgs.lib.nixosSystem {
+              inherit system;
+              pkgs = import nixpkgs {inherit system; overlays = [(overlay system)];};
+              modules = [
+                (./configurations + "/${subdirName}/configuration.nix")
+              ];
+            };
+        in
+        {
+          atlas = nixosSystemFromDir {system = "x86_64-linux"; subdirName = "atlas";};
+          bootstrap-bill = nixosSystemFromDir {system = "x86_64-linux"; subdirName = "bootstrap-bill";};
+          nixos-oblivion = nixosSystemFromDir {system = "x86_64-linux"; subdirName = "nixos-oblivion";};
+          # speet = nixosSystemFromDir {system = "aarch64-linux"; subdirName = "speet";};
+          # weedle = nixosSystemFromDir {system = "x86_64-linux"; subdirName = "weedle";};
+        };
+
+      devShell.x86_64-linux =
+        let
+          pkgs = import nixpkgs {inherit system; overlays = [(overlay "x86_64-linux")];};
+        in pkgs.mkShell {
+          name = "bare-metal-shell";
+          buildInputs = [
+            pkgs.git
+            pkgs.kubectl
+            pkgs.nixos-generators
+            pkgs.vim
+          ];
+        };
+    };
 }
