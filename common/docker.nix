3106e00ee7829eb0b08950f2172e20d0d1202ed5
jali-clarke <GRX01@hotmail.com>
2021-03-14 09:43:48 -0400

    selectively enable docker service

--- common/docker.nix
+++ common/docker.nix
@@ -1,9 +1,35 @@
-{...}: {
-  virtualisation.docker.enable = true;
-  environment.etc."docker/daemon.json".text = ''
+{config, lib, ...}: {
+  options.homelab-config.docker-support =
+    let
+      inherit (lib) types mkOption;
+    in
     {
-      "insecure-registries": ["docker.lan:5000"],
-      "max-concurrent-uploads": 1
-    }
-  '';
+      usingKubernetes = mkOption {
+        type = types.bool;
+        default = false;
+      };
+    };
+
+  config =
+    let
+      cfg = config.homelab-config.docker-support;
+    in
+    lib.mkMerge [
+      (
+        {
+          environment.etc."docker/daemon.json".text = ''
+            {
+              "insecure-registries": ["docker.lan:5000"],
+              "max-concurrent-uploads": 1
+            }
+          '';
+        }
+      )
+
+      (
+        lib.mkIf (!cfg.usingKubernetes) {
+          virtualisation.docker.enable = true;
+        }
+      )
+    ];
 }
