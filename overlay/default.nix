373ff9a5a3c08823af75ab3ff23f48a0a141fb2c
jali-clarke <jinnah.ali-clarke@outlook.com>
2022-01-17 01:57:14 +0000

    remove unnecessary overlay entries for sanoid, agenix

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,12 +1,5 @@
 { nixos-generators, hostname, selfSourceInfo }:
 final: prev: {
   inherit nixos-generators;
-
-  agenix = final.writeShellScriptBin "agenix" ''
-    export EDITOR=${final.vim}/bin/vim
-    exec ${prev.agenix}/bin/agenix "$@"
-  '';
-
-  sanoid = final.callPackage ./sanoid.nix { };
   system-flake-info = import ./system-flake-info.nix { inherit hostname selfSourceInfo; pkgs = final; };
 }
