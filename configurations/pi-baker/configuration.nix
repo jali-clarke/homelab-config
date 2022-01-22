1fe87cf900367ed28897518359375038a3a75b13
jali-clarke <jinnah.ali-clarke@outlook.com>
2022-01-22 18:43:45 +0000

    update pi-baker

--- configurations/pi-baker/configuration.nix
+++ configurations/pi-baker/configuration.nix
@@ -1,11 +1,5 @@
-{ pkgs, lib, ... }:
+{ lib, ... }:
 {
-  imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/sd-image-aarch64.nix>
-    ../../modules/common-config
-    ../../modules/users
-  ];
-
   networking.hostName = "pi-baker"; # Define your hostname.
   systemd.services.sshd.wantedBy = lib.mkOverride 40 [ "multi-user.target" ];
   sdImage.compressImage = false;
