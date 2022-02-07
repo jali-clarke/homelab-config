1939e947666186160314e21ec78465a0fb01d23b
Jinnah Ali-Clarke <jinnah.ali-clarke@outlook.com>
2022-02-06 21:49:30 -0500

    get multiplat devShell with flake-utils

--- flake.nix
+++ flake.nix
@@ -3,8 +3,9 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.homelab-secrets.url = "git+ssh://git@github.com/jali-clarke/homelab-secrets";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, homelab-secrets }:
+  outputs = { self, nixpkgs, homelab-secrets, flake-utils }:
     let
       overlay = { system, hostname }:
         import ./overlay {
@@ -44,91 +45,82 @@
           speet = nixosSystemFromDir { system = "aarch64-linux"; subdirName = "speet"; };
           weedle = nixosSystemFromDir { system = "x86_64-linux"; subdirName = "weedle"; };
         };
+    } // flake-utils.lib.eachDefaultSystem (
+      system: {
+        overlays = final: prev:
+          let
+            pkgs = mkPkgs { inherit system; hostname = "<overlay>"; };
+          in
+          {
+            inherit (pkgs) k9s kubectl nixos-generators;
+          };
 
-      overlays.x86_64-linux = final: prev:
-        let
-          pkgs = mkPkgs { system = "x86_64-linux"; hostname = "<overlay>"; };
-        in
-        {
-          inherit (pkgs) k9s kubectl nixos-generators;
-        };
+        devShell =
+          let
+            pkgs = mkPkgs { inherit system; hostname = "<devShell>"; };
+            meta = import ./lib/get-meta.nix { inherit pkgs; };
 
-      devShell.x86_64-linux =
-        let
-          pkgs = mkPkgs { system = "x86_64-linux"; hostname = "<devShell>"; };
-          meta = import ./lib/get-meta.nix { inherit pkgs; };
+            nixos-generate = "${pkgs.nixos-generators}/bin/nixos-generate";
+            ssh = "${pkgs.openssh}/bin/ssh";
 
-          nixos-generate = "${pkgs.nixos-generators}/bin/nixos-generate";
-          ssh = "${pkgs.openssh}/bin/ssh";
+            buildBootstrapBill = pkgs.writeScriptBin "build_bootstrap_bill" ''
+              #!${pkgs.runtimeShell} -xe
+              ${nixos-generate} -f install-iso --flake '.#bootstrap-bill'
+            '';
 
-          buildBootstrapBill = pkgs.writeScriptBin "build_bootstrap_bill" ''
-            #!${pkgs.runtimeShell} -xe
-            ${nixos-generate} -f install-iso --flake '.#bootstrap-bill'
-          '';
+            buildPiBaker = pkgs.writeScriptBin "build_pi_baker" ''
+              #!${pkgs.runtimeShell} -xe
+              ${nixos-generate} -f sd-aarch64-installer --flake '.#pi-baker'
+            '';
 
-          buildPiBaker = pkgs.writeScriptBin "build_pi_baker" ''
-            #!${pkgs.runtimeShell} -xe
-            ${nixos-generate} -f sd-aarch64-installer --flake '.#pi-baker'
-          '';
+            fetchKubeconfig = pkgs.writeScriptBin "fetch_kubeconfig" ''
+              #!${pkgs.runtimeShell} -e
+              mkdir -p ~/.kube /var/lib/kubernetes/secrets
+              mkdir -p /var/lib/kubernetes/secrets
 
-          fetchKubeconfig = pkgs.writeScriptBin "fetch_kubeconfig" ''
-            #!${pkgs.runtimeShell} -e
-            mkdir -p ~/.kube /var/lib/kubernetes/secrets
-            mkdir -p /var/lib/kubernetes/secrets
+              for target_file in ~/.kube/config /var/lib/kubernetes/secrets/ca.pem /var/lib/kubernetes/secrets/cluster-admin.pem /var/lib/kubernetes/secrets/cluster-admin-key.pem; do
+                if [ -e $target_file ]; then
+                  echo warning! $target_file already exists, moving to $target_file.old
+                  mv $target_file $target_file.old
+                fi
+              done
 
-            for target_file in ~/.kube/config /var/lib/kubernetes/secrets/ca.pem /var/lib/kubernetes/secrets/cluster-admin.pem /var/lib/kubernetes/secrets/cluster-admin-key.pem; do
-              if [ -e $target_file ]; then
-                echo warning! $target_file already exists, moving to $target_file.old
-                mv $target_file $target_file.old
-              fi
-            done
+              ${ssh} pi@${meta.weedle.networkIP} -- sudo cat /etc/kubernetes/cluster-admin.kubeconfig > ~/.kube/config
+              ${ssh} pi@${meta.weedle.networkIP} -- sudo cat /var/lib/kubernetes/secrets/ca.pem > /var/lib/kubernetes/secrets/ca.pem
+              ${ssh} pi@${meta.weedle.networkIP} -- sudo cat /var/lib/kubernetes/secrets/cluster-admin.pem > /var/lib/kubernetes/secrets/cluster-admin.pem
+              ${ssh} pi@${meta.weedle.networkIP} -- sudo cat /var/lib/kubernetes/secrets/cluster-admin-key.pem > /var/lib/kubernetes/secrets/cluster-admin-key.pem
+            '';
 
-            ${ssh} pi@${meta.weedle.networkIP} -- sudo cat /etc/kubernetes/cluster-admin.kubeconfig > ~/.kube/config
-            ${ssh} pi@${meta.weedle.networkIP} -- sudo cat /var/lib/kubernetes/secrets/ca.pem > /var/lib/kubernetes/secrets/ca.pem
-            ${ssh} pi@${meta.weedle.networkIP} -- sudo cat /var/lib/kubernetes/secrets/cluster-admin.pem > /var/lib/kubernetes/secrets/cluster-admin.pem
-            ${ssh} pi@${meta.weedle.networkIP} -- sudo cat /var/lib/kubernetes/secrets/cluster-admin-key.pem > /var/lib/kubernetes/secrets/cluster-admin-key.pem
-          '';
+            updateKnownGood = pkgs.writeScriptBin "update_known_good" ''
+              #!${pkgs.runtimeShell} -e
 
-          updateKnownGood = pkgs.writeScriptBin "update_known_good" ''
-            #!${pkgs.runtimeShell} -e
+              for tag in $(${pkgs.git}/bin/git tag | ${pkgs.gnugrep}/bin/grep known-good); do
+                ${pkgs.git}/bin/git tag -f $tag $1
+              done
 
-            for tag in $(${pkgs.git}/bin/git tag | ${pkgs.gnugrep}/bin/grep known-good); do
-              ${pkgs.git}/bin/git tag -f $tag $1
-            done
+              ${pkgs.git}/bin/git push -f --tag
+            '';
+          in
+          pkgs.mkShell {
+            name = "bare-metal-shell";
+            buildInputs = [
+              pkgs.diffutils
+              pkgs.dnsutils
+              pkgs.git
+              pkgs.kubectl
+              pkgs.kubernetes-helm
+              pkgs.nixos-generators
+              pkgs.nixpkgs-fmt
+              pkgs.openssl
+              pkgs.telnet
+              pkgs.vim
 
-            ${pkgs.git}/bin/git push -f --tag
-          '';
-        in
-        pkgs.mkShell {
-          name = "bare-metal-shell";
-          buildInputs = [
-            pkgs.diffutils
-            pkgs.dnsutils
-            pkgs.git
-            pkgs.kubectl
-            pkgs.kubernetes-helm
-            pkgs.nixos-generators
-            pkgs.nixpkgs-fmt
-            pkgs.openssl
-            pkgs.telnet
-            pkgs.vim
-
-            buildBootstrapBill
-            buildPiBaker
-            fetchKubeconfig
-            updateKnownGood
-          ];
-        };
-
-      devShell.x86_64-darwin =
-        let
-          pkgs = mkPkgs { system = "x86_64-darwin"; hostname = "<devShell>"; };
-        in
-        pkgs.mkShell {
-          name = "bare-metal-shell";
-          buildInputs = [
-            pkgs.nixpkgs-fmt
-          ];
-        };
-    };
+              buildBootstrapBill
+              buildPiBaker
+              fetchKubeconfig
+              updateKnownGood
+            ];
+          };
+      }
+    );
 }
