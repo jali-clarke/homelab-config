45ff72bb89d32379715891ed922231c366b0bfe8
jali-clarke <jinnah.ali-clarke@outlook.com>
2022-02-12 22:04:38 +0000

    add advanced settings write on activation

--- modules/kodi/default.nix
+++ modules/kodi/default.nix
@@ -18,6 +18,20 @@
   config =
     let
       cfg = config.homelab-config.kodi;
+
+      advancedSettings = pkgs.writeText "advancedsettings.xml" ''
+        <advancedsettings version="1.0">
+          <services>
+            <esallinterfaces>true</esallinterfaces>
+            <webserver>true</webserver>
+            <webserverport>${toString cfg.remoteControlPort}</webserverport>
+            <webserverauthentication>false</webserverauthentication>
+            <webserverusername>pi</webserverusername>
+            <webserverpassword></webserverpassword>
+            <webserverssl>false</webserverssl>
+          </services>
+        </advancedsettings>
+      '';
     in
     lib.mkIf cfg.enable {
       sound.enable = true;
@@ -42,6 +56,23 @@
       networking.firewall = {
         allowedTCPPorts = [ cfg.remoteControlPort ];
         allowedUDPPorts = [ cfg.remoteControlPort ];
+      };
+
+      system.activationScripts.writeKodiAdvancedSettings = {
+        deps = [ "users" "groups" ];
+        text = ''
+          USERDATA_DIR="/home/${config.services.xserver.displayManager.autoLogin.user}/.kodi/userdata"
+          ADVANCEDSETTINGS_XML="$USERDATA_DIR/advancedsettings.xml"
+
+          mkdir -p "$USERDATA_DIR"
+          chmod 755 "$USERDATA_DIR"
+          chown pi "$USERDATA_DIR"
+
+          rm -f "$ADVANCEDSETTINGS_XML"
+          cp ${advancedSettings} "$ADVANCEDSETTINGS_XML"
+          chmod 444 "$ADVANCEDSETTINGS_XML"
+          chown pi "$ADVANCEDSETTINGS_XML"
+        '';
       };
     };
 }
