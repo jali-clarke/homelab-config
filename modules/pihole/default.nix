4e1d1b45e59e36fccf4ab925590b487b54f10279
jali-clarke <jinnah.ali-clarke@outlook.com>
2022-03-18 21:12:27 +0000

    define pihole image as derivation to ensure pre-pulling

--- modules/pihole/default.nix
+++ modules/pihole/default.nix
@@ -42,32 +42,52 @@
       piholeLanConf = pkgs.writeText "02-lan.conf" cfg.extraDnsmasqConfig;
     in
     lib.mkIf cfg.enable {
-      virtualisation.oci-containers.containers.pihole = {
-        image = "pihole/pihole:2022.02.1";
+      assertions = [
+        {
+          assertion = pkgs.system == "x86_64-linux";
+          message = "homelab-config.modules.pihole is currently only supported on x86_64-linux";
+        }
+      ];
 
-        environment = {
-          IPv6 = if cfg.ipv6Enabled then "True" else "False";
+      virtualisation.oci-containers.containers.pihole =
+        let
+          imageName = "pihole/pihole";
+          finalImageTag = "2022.02.1";
+
+          pulledImage = pkgs.dockerTools.pullImage {
+            inherit imageName finalImageTag;
+            # warning - below is for the amd64 image.  too lazy to set this up for other archs
+            imageDigest = "sha256:e2dff636c08214c8ca575e924ffbcd2e8e3ce10552c1582390a23e63d486d573";
+            sha256 = "sha256-qm2UmqckWcHJkA1oWKbls4ZRnCPiJ8p9u6P2Z9hc/iY=";
+          };
+        in
+        {
+          image = "${imageName}:${finalImageTag}";
+          imageFile = pulledImage;
+
+          environment = {
+            IPv6 = if cfg.ipv6Enabled then "True" else "False";
+          };
+
+          ports = [
+            "${cfg.webInterface.ip}:${toString cfg.webInterface.port}:80/tcp"
+            "53:53/tcp"
+            "53:53/udp"
+          ];
+
+          volumes = [
+            "${cfg.piholeDataPath}/config:/etc/pihole"
+            "${cfg.piholeDataPath}/dnsmasq:/etc/dnsmasq.d"
+            "${piholeLanConf}:/etc/dnsmasq.d/02-lan.conf:ro"
+          ];
+
+          extraOptions = [
+            "--hostname=pihole"
+            "--dns=127.0.0.1"
+
+            "--cpus=0.1"
+            "--memory=128m"
+          ];
         };
-
-        ports = [
-          "${cfg.webInterface.ip}:${toString cfg.webInterface.port}:80/tcp"
-          "53:53/tcp"
-          "53:53/udp"
-        ];
-
-        volumes = [
-          "${cfg.piholeDataPath}/config:/etc/pihole"
-          "${cfg.piholeDataPath}/dnsmasq:/etc/dnsmasq.d"
-          "${piholeLanConf}:/etc/dnsmasq.d/02-lan.conf:ro"
-        ];
-
-        extraOptions = [
-          "--hostname=pihole"
-          "--dns=127.0.0.1"
-
-          "--cpus=0.1"
-          "--memory=128m"
-        ];
-      };
     };
 }
