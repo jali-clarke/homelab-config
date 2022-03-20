e16f3497953043cb38c9ba9aefc1ec6b42731960
jali-clarke <jinnah.ali-clarke@outlook.com>
2022-03-20 16:39:03 +0000

    add mac addr to host metadata

--- lib/get-meta.nix
+++ lib/get-meta.nix
@@ -2,22 +2,54 @@
 let
   inherit (pkgs) lib;
 
-  ipMap = {
-    atlas = "192.168.0.103";
-    osmc = "192.168.0.104";
-    scribe = "192.168.0.105";
-    speet = "192.168.0.101";
-    spoot = "192.168.0.106";
-    weedle = "192.168.0.102";
+  hostMap = {
+    atlas = {
+      ipv4 = "192.168.0.103";
+      mac = "b4:2e:99:a0:53:3b";
+    };
+
+    osmc = {
+      ipv4 = "192.168.0.104";
+      mac = "dc:a6:32:63:ec:e8";
+    };
+
+    scribe = {
+      ipv4 = "192.168.0.105";
+      mac = "f8:d0:27:6e:65:0e";
+    };
+
+    speet = {
+      ipv4 = "192.168.0.101";
+      mac = "b8:27:eb:3e:38:20";
+    };
+
+    spoot = {
+      ipv4 = "192.168.0.106";
+      mac = "b8:27:eb:a6:61:01";
+    };
+
+    weedle = {
+      ipv4 = "192.168.0.102";
+      mac = "e0:d5:5e:e4:61:74";
+    };
 
     # virtual host on k8s
-    ingress = "192.168.0.200";
-    ingress-external = "192.168.0.202";
+    ingress = {
+      ipv4 = "192.168.0.200";
+      mac = null;
+    };
+
+    ingress-external = {
+      ipv4 = "192.168.0.202";
+      mac = null;
+    };
   };
 
-  mkEntry = hostName: networkIP: {
-    inherit hostName networkIP;
+  mkEntry = hostName: networkInfo: {
+    inherit hostName;
     hostNameWithDomain = "${hostName}.jali-clarke.ca";
+    networkIP = networkInfo.ipv4;
+    macAddress = networkInfo.mac;
   };
 in
-lib.attrsets.mapAttrs mkEntry ipMap
+lib.attrsets.mapAttrs mkEntry hostMap
