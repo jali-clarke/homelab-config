f3126a857dbbf05fb88899672c2d9a803bd1fb6c
jali-clarke <jinnah.ali-clarke@outlook.com>
2022-03-27 19:26:20 +0000

    make 0.0.0.0/0 routable over the vpn but for real now

--- configurations/atlas/configuration.nix
+++ configurations/atlas/configuration.nix
@@ -6,6 +6,10 @@ let
   vaultPort = 8082;
   wireguardPort = 51820;
 
+  # these don't need to be the same, i just didn't want to think of 2 numbers
+  vpnTunnelTableId = "13642";
+  vpnTunnelFwmark = vpnTunnelTableId;
+
   meta = config.homelab-config.meta;
 
   secretFilePi = secretFilePath: {
@@ -215,25 +219,125 @@ in
 
   boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
+  # we'll do everything in nftables since setting networking.nftables.ruleset seems to clobber everything done by docker itself
+  # will have to create nftable rules for each container manually though
+  virtualisation.docker.extraOptions = "--iptables=false";
+
+  networking.nftables = {
+    enable = true;
+    ruleset = ''
+      # vpn stuff here
+
+      table ip vpn-gateway {
+        chain prerouting {
+          type filter hook prerouting priority mangle;
+
+          # mark traffic from the local network destined for vpn gateway with the appropriate fwmark
+          ip saddr 192.168.0.0/24 ip daddr != ${meta.atlas.networkIP} mark set ${vpnTunnelFwmark}
+        }
+      }
+
+      # all docker stuff below
+
+      table ip nat {
+        chain DOCKER {
+          iifname "docker0" counter return
+
+          # add more rules here, for each port
+          iifname != "docker0" meta l4proto tcp ip daddr 127.0.0.1 tcp dport 8081 counter dnat to 172.17.0.2:80
+          iifname != "docker0" meta l4proto tcp tcp dport 53 counter dnat to 172.17.0.2:53
+          iifname != "docker0" meta l4proto udp udp dport 53 counter dnat to 172.17.0.2:53
+        }
+
+        chain POSTROUTING {
+          type nat hook postrouting priority srcnat; policy accept;
+          oifname != "docker0" ip saddr 172.17.0.0/16 counter masquerade
+
+          # add more rules here, for each port
+          meta l4proto tcp ip saddr 172.17.0.2 ip daddr 172.17.0.2 tcp dport 80 counter masquerade 
+          meta l4proto tcp ip saddr 172.17.0.2 ip daddr 172.17.0.2 tcp dport 53 counter masquerade 
+          meta l4proto udp ip saddr 172.17.0.2 ip daddr 172.17.0.2 udp dport 53 counter masquerade 
+        }
+
+        chain PREROUTING {
+          type nat hook prerouting priority dstnat; policy accept;
+          fib daddr type local counter jump DOCKER
+        }
+
+        chain OUTPUT {
+          type nat hook output priority -100; policy accept;
+          ip daddr != 127.0.0.0/8 fib daddr type local counter jump DOCKER
+        }
+      }
+
+      table ip filter {
+        chain DOCKER {
+          # add more rules here, for each port
+          iifname != "docker0" oifname "docker0" meta l4proto tcp ip daddr 172.17.0.2 tcp dport 80 counter accept
+          iifname != "docker0" oifname "docker0" meta l4proto tcp ip daddr 172.17.0.2 tcp dport 53 counter accept
+          iifname != "docker0" oifname "docker0" meta l4proto udp ip daddr 172.17.0.2 udp dport 53 counter accept
+        }
+
+        chain DOCKER-ISOLATION-STAGE-1 {
+          iifname "docker0" oifname != "docker0" counter jump DOCKER-ISOLATION-STAGE-2
+          counter return
+        }
+
+        chain DOCKER-ISOLATION-STAGE-2 {
+          oifname "docker0" counter drop
+          counter return
+        }
+
+        chain FORWARD {
+          type filter hook forward priority filter; policy accept;
+          counter jump DOCKER-USER
+          counter jump DOCKER-ISOLATION-STAGE-1
+          oifname "docker0" ct state related,established counter accept
+          oifname "docker0" counter jump DOCKER
+          iifname "docker0" oifname != "docker0" counter accept
+          iifname "docker0" oifname "docker0" counter accept
+        }
+
+        chain DOCKER-USER {
+          counter return
+        }
+      }
+    '';
+  };
+
   networking.wireguard = {
     enable = true;
-    interfaces.wg-homelab = {
-      # we don't want to route 0.0.0.0/0 by default,
-      # just for connections coming in from 192.168.0.4
-      allowedIPsAsRoutes = false;
-      ips = [ "192.168.0.4/24" "192.168.128.2/24" ];
-      privateKeyFile = config.age.secrets."wg_client_gateway_key".path;
+    interfaces.wg-homelab =
+      let
+        ip = "${pkgs.iproute2}/bin/ip";
+      in
+      {
+        # we don't want to route 0.0.0.0/0 by default,
+        # just for connections coming in from 192.168.0.4
+        allowedIPsAsRoutes = false;
+        ips = [ "192.168.0.4/24" "192.168.128.2/24" ];
+        privateKeyFile = config.age.secrets."wg_client_gateway_key".path;
 
-      peers = [
-        {
-          # server
-          publicKey = "au8MlRKPPYaPJ4N4bISWnClNo5sS0DSf7EJBAUYJqkA=";
-          allowedIPs = [ "0.0.0.0/0" ];
-          endpoint = "cerberus.jali-clarke.ca:${builtins.toString wireguardPort}";
-          persistentKeepalive = 25;
-        }
-      ];
-    };
+        postSetup = ''
+          ${ip} route add default via 192.168.128.1 dev wg-homelab table ${vpnTunnelTableId}
+          ${ip} rule add fwmark ${vpnTunnelFwmark} table ${vpnTunnelTableId}
+        '';
+
+        postShutdown = ''
+          ${ip} rule delete fwmark ${vpnTunnelFwmark} table ${vpnTunnelTableId}
+          ${ip} route delete default via 192.168.128.1 dev wg-homelab table ${vpnTunnelTableId}
+        '';
+
+        peers = [
+          {
+            # server
+            publicKey = "au8MlRKPPYaPJ4N4bISWnClNo5sS0DSf7EJBAUYJqkA=";
+            allowedIPs = [ "0.0.0.0/0" ];
+            endpoint = "cerberus.jali-clarke.ca:${builtins.toString wireguardPort}";
+            persistentKeepalive = 25;
+          }
+        ];
+      };
   };
 
   # This value determines the NixOS release from which the default
